c55d1393f537597df8aa7f6c1d79b6af12d988dd
DeboDevelop <debajyotidofficial@gmail.com>
2024-02-07 19:57:09 +0530

    feat: Added feature for blacklisting words

--- src/config.py
+++ src/config.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
+    FLASK_ENV = os.getenv('FLASK_ENV')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):
-    TESTING = True
+    FLASK_ENV = 'test'
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_TEST_DATABASE_URI')
--- src/services/wikiWordCount/word_frequency.py
+++ src/services/wikiWordCount/word_frequency.py
@@ -1,8 +1,27 @@
+import os
+import yaml
+
 from collections import defaultdict
 from queue import PriorityQueue
 from typing import Tuple, List
 
 from wikipediaapi import Wikipedia
+from config import Config
+
+def blacklist_words(word_frequency: dict) -> dict:
+    env = Config.FLASK_ENV
+    yaml_path = os.path.join(os.path.dirname(__file__), f'../../flags/{env}.yaml')
+
+    with open(yaml_path, 'r') as file:
+        config = yaml.safe_load(file)
+        feature_flag = config.get('feature_flag', [])
+        blacklisted_words = config.get('blacklisted_words', [])
+
+        if feature_flag:
+            for word in blacklisted_words:
+                del word_frequency[word]
+
+    return word_frequency    
 
 def get_word_frequency(topic: str, n: int) -> Tuple[List[Tuple[str, int]], bool]:
     # Fetch the Wikipedia article text
@@ -19,18 +38,20 @@ def get_word_frequency(topic: str, n: int) -> Tuple[List[Tuple[str, int]], bool]
     word_frequency = defaultdict(int)
     for word in words:
         word_frequency[word] += 1
-        
-    min_heap = PriorityQueue()
+    
+    word_frequency = blacklist_words(word_frequency)
+
+    pq = PriorityQueue()
 
     for word, count in word_frequency.items():
-        min_heap.put((count, word))
-        if min_heap.qsize() > n:
-            min_heap.get()
+        pq.put((count, word))
+        if pq.qsize() > n:
+            pq.get()
 
-    top_words = [(None, None)] * min_heap.qsize()
+    top_words = [(None, None)] * pq.qsize()
 
     for i in range(len(top_words) - 1, -1, -1):
-        count, word = min_heap.get()
+        count, word = pq.get()
         top_words[i] = (word, count)
 
     return top_words, True
