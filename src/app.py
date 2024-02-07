7aff53dab2efcc0450fb0b0fd497fd66e4f01253
DeboDevelop <debajyotidofficial@gmail.com>
2024-02-07 17:33:11 +0530

    feat: Added Search Model

--- src/app.py
+++ src/app.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from services.wikiWordCount.routes.api import api_bp
 from middleware import log_requests
-
-db = SQLAlchemy()
+from services.wikiWordCount.models.search import Search
+from utils.db import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(api_bp, url_prefix='/api')
 
---
+++ src/services/wikiWordCount/models/search.py
@@ -0,0 +1,11 @@
+from utils.db import db
+from sqlalchemy.sql import func
+
+class Search(db.Model):
+    __tablename__ = 'searches'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    topic = db.Column(db.String, nullable=False)
+    n = db.Column(db.Integer, nullable=False)
+    top_words = db.Column(db.JSON, nullable=True)
+    timestamp = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
